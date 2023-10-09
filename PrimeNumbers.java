1a849794a20d35587ae1cbb79593dd091dff8bb9
sumanthraj01 <147322578+sumanthraj01@users.noreply.github.com>
2023-10-09 15:41:57 +0530

    PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,24 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PrimeNumbers {
     public static List<Integer> findPrimes(int n) {
         List<Integer> primes = new ArrayList<>();
-        boolean[] isPrime = new boolean[n + 1];
         for (int i = 2; i <= n; i++) {
-            isPrime[i] = true;
-        }
-        
-        for (int p = 2; p * p <= n; p++) {
-            if (isPrime[p]) {
-                for (int i = p * p; i <= n; i += p) {
-                    isPrime[i] = false;
+            boolean isPrime = true;
+            for (int j = 2; j < i/2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
                 }
             }
-        }
-        
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
+            if (isPrime) {
                 primes.add(i);
             }
         }
